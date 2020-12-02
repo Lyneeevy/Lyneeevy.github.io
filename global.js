b67af74ef28ab816fdf9d96bead143d94beeb18a
Ktan <fredericmamath@gmail.com>
2020-12-02 22:59:19 +0100

    :bug: Fixes dropdown

--- global.js
+++ global.js
@@ -1,20 +1,23 @@
 // Masquer tous les restaurants non className
 function hideRestaurantsNot(className) {
+  console.log('hello', className);
 
-// Masquer tous les restaurants
-var restaurants = document.getElementsByClassName("restaurant");
-var i;
-for (i = 0; i <restaurants.length; i++) {
-  restaurants[i].classList.add("hidden");
-  restaurants[i].classList.remove("show");
-}
+  // Masquer tous les restaurants
+  var restaurants = document.getElementsByClassName("goodAddress");
+  var i;
+  for (i = 0; i <restaurants.length; i++) {
+    restaurants[i].classList.add("hidden");
+    restaurants[i].classList.remove("show");
+  }
 
-// Afficher les restaurants className
-var restaurantsSelectionnes = document.getElementsByClassName(className);
-var i;
-for (i = 0; i <restaurantsSelectionnes.length; i++) {
-  restaurantsSelectionnes[i].classList.add("show");
-}
+  // Afficher les restaurants className
+  console.log({ className });
+  var restaurantsSelectionnes = document.getElementsByClassName(className);
+  var i;
+  for (i = 0; i < restaurantsSelectionnes.length; i++) {
+    restaurantsSelectionnes[i].classList.add("show");
+    restaurantsSelectionnes[i].classList.remove("hidden");
+  }
 }
 
 //Get the button scrollToTop
@@ -57,7 +60,7 @@ stickNavbarToTop();
 
 function showAllRestaurants() {
 
-var allRestaurants = document.getElementsByClassName("restaurant");
+var allRestaurants = document.getElementsByClassName("goodAddress");
 var i;
 for (i = 0; i <allRestaurants.length; i++) {
   allRestaurants[i].classList.add("show");
@@ -81,5 +84,5 @@ menuDropdown.style.display = "block";
 
 function hideRestaurantsNotAndHideNavigationItem(className,id) {
 hideRestaurantsNot(className);
-hideMenuDropdown(id);
+// hideMenuDropdown(id);
 }
