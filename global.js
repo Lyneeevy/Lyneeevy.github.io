a8a5e63c0d9015e5d6a005bf834885cc6aacd042
Lyneeevy <evelyne.phich@gmail.com>
2020-12-04 23:00:08 +0100

    Création de pages

--- global.js
+++ global.js
@@ -1,25 +1,3 @@
-// Masquer tous les restaurants non className
-function hideRestaurantsNot(className) {
-  console.log("hello", className);
-
-  // Masquer tous les restaurants
-  var restaurants = document.getElementsByClassName("goodAddress");
-  var i;
-  for (i = 0; i < restaurants.length; i++) {
-    restaurants[i].classList.add("hidden");
-    restaurants[i].classList.remove("show");
-  }
-
-  // Afficher les restaurants className
-  console.log({ className });
-  var restaurantsSelectionnes = document.getElementsByClassName(className);
-  var i;
-  for (i = 0; i < restaurantsSelectionnes.length; i++) {
-    restaurantsSelectionnes[i].classList.add("show");
-    restaurantsSelectionnes[i].classList.remove("hidden");
-  }
-}
-
 //Get the button scrollToTop
 var scrollToTopButton = document.getElementById("scrollToTop");
 
@@ -57,44 +35,3 @@ window.onscroll = function () {
   scrollToTopButtonDisplay();
   stickNavbarToTop();
 };
-
-function showAllRestaurants() {
-  var allRestaurants = document.getElementsByClassName("goodAddress");
-  var i;
-  for (i = 0; i < allRestaurants.length; i++) {
-    allRestaurants[i].classList.add("show");
-    allRestaurants[i].classList.remove("hidden");
-  }
-  // hideMenuDropdown(id);
-}
-
-// A EFFACER
-
-function hideMenuDropdown(id) {
-  // Cache le navigation item dans le dropdown quand on clique sur l'id
-  var menuDropdown = document.getElementById(id);
-
-  menuDropdown.style.display = "none";
-}
-
-function showMenuDropdown(id) {
-  // Affiche le menu dropwdown quand on clique sur le dropdown toggler
-  var menuDropdown = document.getElementById(id);
-
-  menuDropdown.style.display = "block";
-}
-
-function hideRestaurantsNotAndHideNavigationItem(className, id) {
-  hideRestaurantsNot(className);
-  // hideMenuDropdown(id);
-}
-//
-
-$(document).ready(function () {
-  // jQuery methods go here...
-  $("#toutesLesAdresses").click(function () {});
-  $("#toutesLesAdresses").attr("data-toggle", "collapse");
-  $("#toutesLesAdresses").attr("data-target", ".navbar-collapse");
-  $("#lien2").attr("data-toggle", "collapse");
-  $("#lien2").attr("data-target", ".navbar-collapse");
-});
